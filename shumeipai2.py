9a1cfc82c012e7f36ccd6bd1e5c55ae180c89611
qiusanjiaohanshu <125940469+qiusanjiaohanshu@users.noreply.github.com>
2023-11-09 22:27:40 +0800

    Add files via upload

---
+++ shumeipai2.py
@@ -0,0 +1,108 @@
+import socket
+import threading
+import cv2
+import RPi.GPIO as GPIO
+import time
+
+IN1 = 11  # IN1, 左前进 
+IN2 = 12  # IN2, 左后退 
+IN3 = 16  # IN3, 右前进 
+IN4 = 15  # IN4, 右后退 
+# 设置服务器的IP地址和端口
+HOST = '1.14.63.108'  # 服务器的IP地址
+PORT = 5000
+
+# 创建 socket 对象
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+# 初始化昵称
+nickname = "shumaipai"
+client_socket.sendall(nickname.encode("utf-8"))
+
+def init():
+    
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(IN1,GPIO.OUT)
+    GPIO.setup(IN2,GPIO.OUT)
+    GPIO.setup(IN3,GPIO.OUT)
+    GPIO.setup(IN4,GPIO.OUT)
+
+def forward(sleep_time):
+    init()
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,GPIO.HIGH)
+    GPIO.output(IN4,GPIO.LOW)
+    time.sleep(sleep_time)
+    GPIO.cleanup()
+ 
+def back(sleep_time):
+    init()
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.HIGH)
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.HIGH)
+    time.sleep(sleep_time)
+    GPIO.cleanup()
+ 
+def left(sleep_time):
+    init()
+    GPIO.output(IN1,False)
+    GPIO.output(IN2,False)
+    GPIO.output(IN3,GPIO.HIGH)
+    GPIO.output(IN4,GPIO.LOW)
+    time.sleep(sleep_time)
+    GPIO.cleanup()
+ 
+def right(sleep_time):
+    init()
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(IN3,False)
+    GPIO.output(IN4,False)
+    time.sleep(sleep_time)
+    GPIO.cleanup()
+# 接收服务器发送的消息
+def receive_message():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode("utf-8")
+            if "l" in message:
+                left(1)
+                print(message)
+            elif "r" in message:
+                right(1)
+                print(message)
+            elif "f" in message:
+                forward(5)
+                print(message)
+            elif "b" in message:
+                back(1)
+                print(message)
+        except Exception as e:
+            print(f"Error: {e}")
+            client_socket.close()
+            break
+
+# 发送消息给服务器
+def send_message():
+    while True:
+        message = input()
+        client_socket.sendall(message.encode("utf-8"))
+
+# 拍照并保存
+def take_picture():
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    if ret:
+        cv2.imwrite('captured_image.jpg', frame)
+        print("照片已保存为 captured_image.jpg")
+    cap.release()
+
+# 创建线程处理发送和接收消息
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+
+send_thread = threading.Thread(target=send_message)
+send_thread.start()
